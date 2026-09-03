e1791037244f4044891eb81dc7c8f0c550cf29a6
lakshay-aggarwal-codes <08817711624_aiml@vipstc.edu.in>
2026-09-04 00:27:31 +0530

    part 2 added

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/", (req, res) => {
   res.send("namaste");
--- models/listing.js
+++ models/listing.js
@@ -6,33 +6,33 @@ const listingSchema = new Schema({
     type: String,
     required: true,
   },
+
   description: {
     type: String,
   },
-  //   image: {
-  //     type: String,
-  // default:
-  //   "https://www.magnific.com/free-vector/hand-drawn-flat-design-digital-detox-illustration_23997843.htm#fromView=keyword&page=1&position=1&uuid=06442459-8f98-456a-99c7-1c5177af84f1&track=ais_hybrid&query=No+device+found",
-  // set: (v) =>
-  //   v === ""
-  // ? "https://www.magnific.com/free-vector/hand-drawn-flat-design-digital-detox-illustration_23997843.htm#fromView=keyword&page=1&position=1&uuid=06442459-8f98-456a-99c7-1c5177af84f1&track=ais_hybrid&query=No+device+found"
-  //     : v,
-  //   },
 
   image: {
-    filename: String,
-    url: String,
+    filename: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
   },
+
   price: {
     type: Number,
   },
+
   location: {
     type: String,
   },
+
   country: {
     type: String,
   },
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
+
 module.exports = Listing;
