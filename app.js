a536fd2d6fde8726e8c422b199d074becc2231a7
lakshay-aggarwal-codes <08817711624_aiml@vipstc.edu.in>
2026-09-12 12:06:58 +0530

    add review routes

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const Review = require("./models/review.js");
+const mongoose = require("mongoose"); 
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError");
-const wrapAsync = require("./utils/wrapAsync");
-const { listingSchema, reviewSchema } = require("./schema.js");
+const ExpressError = require("./utils/ExpressError"); 
 const listings = require("./routes/listing");
+const reviews = require("./routes/review");
 
 const app = express();
-const PORT = 8080;
+const PORT = 8080; 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/dimora";
 
@@ -40,47 +37,9 @@ app.get("/", (req, res) => {
   res.send("namaste");
 });
  
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else next();
-};
  
 app.use("/listings", listings);
-
-//REVIEWS
-//post
-app.post(
-  "/listings/:id/reviews",
-  validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    console.log("New Review saved");
-
-    res.redirect(`/listings/${listing._id}`);
-  }),
-);
-
-//DELETE review ROutE
-app.delete(
-  "/listings/:id/reviews/:reviewId",
-  wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findById(reviewId);
-    res.redirect(`/listings/${id}`);
-  }),
-);
+app.use("/listings/:id/reviews", reviews);
 
 app.all("/{*splat}", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
@@ -88,8 +47,7 @@ app.all("/{*splat}", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render("error.ejs", { err });
-  // res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs", { err }); 
 });
 
 app.listen(PORT, () => {
